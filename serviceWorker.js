bcb8f25d03355fbcb76a85bc0dcb982207928c42
* <rakara.abila@gmail.com>
2020-05-04 08:37:21 +0300

    jquerymobile ui

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,9 +1,12 @@
-const staticDevCoffee = "dev-coffee-site-v1";
+const staticClimorFresh = "dev-coffee-site-v1";
 const assets = [
   "/",
   "/index.html",
+  "css/jquery.mobile-1.4.5.min.css",
   "/css/style.css",
   "/js/app.js",
+  "js/jquery.min.js",
+  "js/jquery.mobile-1.4.5.min.js",
   "/images/coffee1.jpg",
   "/images/coffee2.jpg",
   "/images/coffee3.jpg",
@@ -17,7 +20,7 @@ const assets = [
 
 self.addEventListener("install", installEvent => {
   installEvent.waitUntil(
-    caches.open(staticDevCoffee).then(cache => {
+    caches.open(staticClimorFresh).then(cache => {
       cache.addAll(assets);
     })
   );
