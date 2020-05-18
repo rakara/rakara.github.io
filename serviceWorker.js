bdc81affb56e8a3fd9e0a31d14f1269207455bae
rakara <rakara.abila@gmail.com>
2020-05-18 23:50:03 +0300

    Added Icons

--- serviceWorker.js
+++ serviceWorker.js
@@ -7,6 +7,15 @@ const assets = [
   "/js/app.js",
   "/js/jquery.min.js",
   "/js/jquery.mobile-1.4.5.min.js",
+  "/images/icons/icon72x72.png",
+  "/images/icons/icon72x72.png"
+  "/images/icons/icon96x96.png",
+  "/images/icons/icon128x128.png",
+  "/images/icons/icon144x144.png",
+  "/images/icons/icon152x152.png",
+  "/images/icons/icon192x192.png",
+  "/images/icons/icon384x384.png",
+  "/images/icons/icon512x512.png",
 ];
 
 self.addEventListener("install", installEvent => {
