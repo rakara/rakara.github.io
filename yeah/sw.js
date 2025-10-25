dd1ff80f78d472471bc83a20a778cabeeb1e099d
Rakara Abila <rakara.abila@gmail.com>
2025-10-25 07:56:58 +0300

    Update sw.js

--- yeah/sw.js
+++ yeah/sw.js
@@ -1,8 +1,8 @@
 const CACHE_NAME = 'yeah-network-v1';
 const urlsToCache = [
   '/',
-  '/yeah/index.html',
-  '/yeah/manifest.json'
+  '/index.html',
+  '/manifest.json'
 ];
 
 self.addEventListener('install', function(event) {
