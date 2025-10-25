e99c31e4dc9751420bf2fd9b2561439efc9d9afd
Rakara Abila <rakara.abila@gmail.com>
2025-10-25 10:30:26 +0300

    Update sw.js

--- yeah/sw.js
+++ yeah/sw.js
@@ -1,8 +1,12 @@
-const CACHE_NAME = 'yeah-network-v1';
+const CACHE_NAME = 'yeah-network-v2';
 const urlsToCache = [
-  '/yeah/',
-  '/yeah/index.html',
-  '/yeah/manifest.json'
+  './',
+  './index.html',
+  './manifest.json',
+  './data/users.json',
+  './data/jobs.json', 
+  './data/categories.json',
+  './data/locations.json'
 ];
 
 self.addEventListener('install', function(event) {
@@ -18,11 +22,47 @@ self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request)
       .then(function(response) {
+        // Return cached version or fetch from network
         if (response) {
           return response;
         }
-        return fetch(event.request);
-      }
-    )
+        
+        return fetch(event.request).then(function(response) {
+          // Check if we received a valid response
+          if(!response || response.status !== 200 || response.type !== 'basic') {
+            return response;
+          }
+
+          // Clone the response
+          var responseToCache = response.clone();
+
+          caches.open(CACHE_NAME)
+            .then(function(cache) {
+              cache.put(event.request, responseToCache);
+            });
+
+          return response;
+        });
+      })
+      .catch(function() {
+        // If both cache and network fail, show offline page
+        if (event.request.url.indexOf('.html') > -1) {
+          return caches.match('./offline.html');
+        }
+      })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
 });
